f6312d1992d58783a10e35579569d6e3e82eaa77
Andre Lucas Ferreira <alucas3103@outlook.com>
2026-05-12 19:38:30 -0300

    init: backend

---
+++ app/src/test/java/com/nativo/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nativo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
