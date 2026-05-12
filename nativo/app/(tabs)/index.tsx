ed27fee1d64e12e56ef8799ddcbfdebdadf8c14a
Leonardo <rodriguesleona38@gmail.com>
2026-05-12 19:14:41 -0300

    telas front

---
+++ nativo/app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#000', // Fundo preto
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
---
+++ nativo/app/(tabs)/premios.tsx
@@ -0,0 +1,20 @@
+
+import { House } from "lucide-react-native";
+import { Text, View } from "react-native";
+
+
+export default function  Premios() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+
+      <House size={48} color="#CE0000" style={{ marginBottom: 16 }} />
+      <Text>Edit app/premios.tsx to edit this screen.</Text>
+    </View>
+  );
+}
